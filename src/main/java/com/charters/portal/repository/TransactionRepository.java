835ae7e8debafa0fa74342a068c90ad4859a6c56
PratapMattam <64245437+PratapMattam@users.noreply.github.com>
2020-04-24 01:56:29 -0500

    Update TransactionRepository.java

--- src/main/java/com/charters/portal/repository/TransactionRepository.java
+++ src/main/java/com/charters/portal/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.charters.portal.repository;
 
 import com.charters.portal.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-	List<Transaction> findByCustomerIdAndTransactionDateGreaterThan(@Param(value = "customer_id") Integer customerId, @Param(value = "transaction_date") LocalDate transactionDate);
-	List<Transaction> findByCustomerId( Integer customerId);
+	List<Transaction> findByCustomerIdAndTransactionDateGreaterThan(Integer customerId, LocalDate transactionDate);
+	List<Transaction> findByCustomerId(Integer customerId);
 }
